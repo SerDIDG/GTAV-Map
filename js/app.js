b19ad8b9d40a496b06cbb14a4f6ab72a4b6f96a8
Serhio Magpie <serdidg@gmail.com>
2018-11-19 01:10:54 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 	var showCoordinations = true;
 
-	if (window.location.protocol != "http:") {
+	if (window.location.protocol != "https:") {
 	    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);		
 	}
 
